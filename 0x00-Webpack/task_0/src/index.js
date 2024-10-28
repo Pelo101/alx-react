0db07277e10d773b6a4ceb34808a3b37cd06a9d1
Pelo101 <110992136+Pelo101@users.noreply.github.com>
2024-10-28 16:11:13 +0200

    src

---
+++ 0x00-Webpack/task_0/src/index.js
@@ -0,0 +1,6 @@
+import $ from 'jquery';
+
+$('body').append('<p>Holberton Dashboard</p>')
+$('body').append('<p>Dashboard for the students</p>')
+$('body').append('<p>Copyright-Holberton School</p>')
+
