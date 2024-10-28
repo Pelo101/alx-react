367f767327d59f28e3cadcb5b928db15387d3401
Pelo101 <110992136+Pelo101@users.noreply.github.com>
2024-10-28 16:36:48 +0200

    edit

--- 0x00-Webpack/task_0/src/index.js
+++ 0x00-Webpack/task_0/src/index.js
@@ -2,5 +2,5 @@ import $ from 'jquery';
 
 $('body').append('<p>Holberton Dashboard</p>')
 $('body').append('<p>Dashboard for the students</p>')
-$('body').append('<p>Copyright-Holberton School</p>')
+$('body').append('<p>Copyright - Holberton School</p>')
 
